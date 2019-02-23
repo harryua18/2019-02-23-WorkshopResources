9b63e510179165cf8caaa386ef859f6765d1ca60
harryua18 <harshadkalyankar@email.arizona.edu>
2019-02-23 16:32:50 -0700

    End of day 1- 022219 data carpentary workshop

---
+++ repository/scripts/loop_conditional1.sh
@@ -0,0 +1,13 @@
+# $1 is field to cut
+# $2 specifies heads or tail
+
+for gapminderfile in [U-Z]*.txt
+do
+    echo $gapminderfile
+    if [ "$2" = "high" ]  # test whether 2nd argument is "high"
+    then
+            cut -f "$1" $gapminderfile | sort -n | tail -1
+    else
+            cut -f "$1" $gapminderfile | sort -n | head -1
+    fi # Dont forget to end the if conditional
+done
